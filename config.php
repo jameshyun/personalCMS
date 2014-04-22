85b443f6165f927184c3d0231f5632d1b34baff0
Jungsik Hyun <sydjung@gmail.com>
2014-04-22 17:20:57 +1000

    configurations for DB connection

---
+++ config.php
@@ -0,0 +1,39 @@
+<?php
+
+
+/*
+ *	Database configuration
+ *  - server
+ *  - username, password
+ *  - DB name
+ */
+
+define("SERVER", "localhost"); 
+
+define("USERNAME", "root"); 
+
+define("PASSWORD", ""); 
+
+define("DATABASE", "js_blog"); 
+/*
+define("SERVER", "mysql.hostinger.kr"); 
+
+define("USERNAME", "u911245505_db"); 
+
+define("PASSWORD", "jovirock23"); 
+
+define("DATABASE", "u911245505_db"); */
+
+/*
+// Connect to DB
+$mysqli = new mysqli(SERVER, USERNAME, PASSWORD, DATABASE);
+
+if(mysqli_connect_errno()) {
+
+echo "Error: Could not connect to database.";
+
+exit;
+
+}*/
+
+
